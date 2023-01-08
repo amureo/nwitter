157921fa1c4a2bde8f6ce387a66e82c265f3efc3
amureo <wkeehd12@gmail.com>
2023-01-08 14:26:21 +0900

    ch5 complete

---
+++ src/components/Router.js
@@ -0,0 +1,26 @@
+import {HashRouter, /*Navigate, */ Route, Routes } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
+
+const AppRouter=({isLoggedIn,userObj})=>{
+    return(
+        <HashRouter>
+            {isLoggedIn && <Navigation />}
+            <Routes>
+                {isLoggedIn?(
+                    <>
+                        <Route path="/" element={<Home userObj={userObj} />} />
+                        <Route path="/profile" element={<Profile />} />
+                    </>
+                ):(
+                    <Route path="/" element={<Auth />} />
+                )}
+                {/* <Route path="*" element={<Navigate to ="/" />}/>  */}
+            </Routes>
+        </HashRouter>
+    );
+};
+
+export default AppRouter;
